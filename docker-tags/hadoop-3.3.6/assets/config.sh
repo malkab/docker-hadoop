e98a0ff7ba26c36ff971db6b2dc1155ec2841001
Malkab <jp.perez.alcantara@gmail.com>
2023-07-20 19:49:09 +0200

    wip

--- docker-tags/hadoop-3.3.6/assets/config.sh
+++ docker-tags/hadoop-3.3.6/assets/config.sh
@@ -1,70 +1,70 @@
 #!/bin/bash
 
-DEBIAN_FRONTEND noninteractive
+# DEBIAN_FRONTEND noninteractive
 
-apt-get update
+# apt-get update
 
-# Install debconf-utils
-apt-get install -y debconf-utils
+# # Install debconf-utils
+# apt-get install -y debconf-utils
 
-apt-get install -y \
-  libreadline5 \
-  locales \
-  tzdata
+# apt-get install -y \
+#   libreadline5 \
+#   locales \
+#   tzdata
 
-dpkg-reconfigure --frontend noninteractive tzdata
+# dpkg-reconfigure --frontend noninteractive tzdata
 
-# Locales
-sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# en_US.ISO-8859-15 ISO-8859-15/en_US.ISO-8859-15 ISO-8859-15/' /etc/locale.gen
-sed -i -e 's/# en_GB ISO-8859-1/en_GB ISO-8859-1/' /etc/locale.gen
-sed -i -e 's/# en_GB.ISO-8859-15 ISO-8859-15/en_GB.ISO-8859-15 ISO-8859-15/' /etc/locale.gen
-sed -i -e 's/# en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# es_ES ISO-8859-1/es_ES ISO-8859-1/' /etc/locale.gen
-sed -i -e 's/# es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# es_ES@euro ISO-8859-15/es_ES@euro ISO-8859-15/' /etc/locale.gen
-sed -i -e 's/# de_DE ISO-8859-1/de_DE ISO-8859-1/' /etc/locale.gen
-sed -i -e 's/# de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# de_DE@euro ISO-8859-15/de_DE@euro ISO-8859-15/' /etc/locale.gen
-sed -i -e 's/# fr_FR ISO-8859-1/fr_FR ISO-8859-1/' /etc/locale.gen
-sed -i -e 's/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# fr_FR@euro ISO-8859-15/fr_FR@euro ISO-8859-15/' /etc/locale.gen
-sed -i -e 's/# it_IT ISO-8859-1/it_IT ISO-8859-1/' /etc/locale.gen
-sed -i -e 's/# it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# it_IT@euro ISO-8859-15/it_IT@euro ISO-8859-15/' /etc/locale.gen
+# # Locales
+# sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+# sed -i -e 's/# es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
+# sed -i -e 's/# en_US.ISO-8859-15 ISO-8859-15/en_US.ISO-8859-15 ISO-8859-15/' /etc/locale.gen
+# sed -i -e 's/# en_GB ISO-8859-1/en_GB ISO-8859-1/' /etc/locale.gen
+# sed -i -e 's/# en_GB.ISO-8859-15 ISO-8859-15/en_GB.ISO-8859-15 ISO-8859-15/' /etc/locale.gen
+# sed -i -e 's/# en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen
+# sed -i -e 's/# es_ES ISO-8859-1/es_ES ISO-8859-1/' /etc/locale.gen
+# sed -i -e 's/# es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
+# sed -i -e 's/# es_ES@euro ISO-8859-15/es_ES@euro ISO-8859-15/' /etc/locale.gen
+# sed -i -e 's/# de_DE ISO-8859-1/de_DE ISO-8859-1/' /etc/locale.gen
+# sed -i -e 's/# de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
+# sed -i -e 's/# de_DE@euro ISO-8859-15/de_DE@euro ISO-8859-15/' /etc/locale.gen
+# sed -i -e 's/# fr_FR ISO-8859-1/fr_FR ISO-8859-1/' /etc/locale.gen
+# sed -i -e 's/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
+# sed -i -e 's/# fr_FR@euro ISO-8859-15/fr_FR@euro ISO-8859-15/' /etc/locale.gen
+# sed -i -e 's/# it_IT ISO-8859-1/it_IT ISO-8859-1/' /etc/locale.gen
+# sed -i -e 's/# it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/' /etc/locale.gen
+# sed -i -e 's/# it_IT@euro ISO-8859-15/it_IT@euro ISO-8859-15/' /etc/locale.gen
 
-# Copy the keyboard configuration
-debconf-set-selections < /keyboard_selections.conf
+# # Copy the keyboard configuration
+# debconf-set-selections < /keyboard_selections.conf
 
-apt-get install -y \
-  apt-utils \
-  vim \
-  mlocate
+# apt-get install -y \
+#   apt-utils \
+#   vim \
+#   mlocate
 
-apt-get -y upgrade
+# apt-get -y upgrade
 
-ldconfig
+# ldconfig
 
-# Clean up
-rm -rf /var/lib/apt/lists/*
-rm /keyboard_selections.conf
+# # Clean up
+# rm -rf /var/lib/apt/lists/*
+# rm /keyboard_selections.conf
 
-update-locale LANG=$LOCALE
+# update-locale LANG=$LOCALE
 
-locale-gen
+# locale-gen
 
-ldconfig
+# ldconfig
 
-# Lastest IPython has a bug in autocompletion, so installing last known working
-# one
-pip install \
-  virtualenv \
-  readline \
-  ipython
+# # Lastest IPython has a bug in autocompletion, so installing last known working
+# # one
+# pip install \
+#   virtualenv \
+#   readline \
+#   ipython
 
-# Some ln -s
-ln -s /usr/bin/ipython3 /usr/bin/ipython
+# # Some ln -s
+# ln -s /usr/bin/ipython3 /usr/bin/ipython
 
 # --------
 #
--- docker-tags/hadoop-3.3.6/tests-namenode_datanode/test_scripts/hdfs.sh
+++ docker-tags/hadoop-3.3.6/tests-namenode_datanode/test_scripts/hdfs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #HDFS_ROOT=hdfs://helios:9022
-HDFS_ROOT=hdfs://kepler:9022
+HDFS_ROOT=hdfs://kepler:9000
 
 /hadoop/bin/hdfs dfs -mkdir $HDFS_ROOT/user
 /hadoop/bin/hdfs dfs -rm -r $HDFS_ROOT/user/test_data_to_put
